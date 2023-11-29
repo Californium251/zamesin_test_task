7d1de4aca861d320683c649bcefa80db1de7af0e
Maxim Kurepov <californium251@gmail.com>
2023-11-29 13:20:32 +0400

    main logic implemented

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
 require('dotenv').config();
 const express = require('express');
-const axios = require('axios');
 const Airtable = require('airtable');
 const app = express();
 const port = process.env.PORT || 3000;
 
 const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(process.env.AIRTABLE_BASE_ID);
+const tableName = process.env.TABLE_NAME || 'Table 1';
 
-const checkIfUserExists = async (email) => base('Table 1').select({
-    view: 'Grid view',
-    maxRecords: 1,
-    filterByFormula: `{Email} = '${email}'`,
-}).firstPage((err, records) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    if (records.length > 0) {
-        console.log(`User with email ${email} exists.`);
-    } else {
-        console.log(`User with email ${email} does not exist.`);
-    }
-});
+const checkIfUserExists = async (email) => {
+    return new Promise((resolve, reject) => {
+        base(tableName).select({
+            view: 'Grid view',
+            maxRecords: 1,
+            filterByFormula: `{Email} = '${email}'`,
+        }).firstPage((err, records) => {
+            if (err) {
+                reject(err.message);
+                return;
+            }
+            resolve(records.length > 0);
+        })
+    });
+};
 
 app.get('/check-user', async (req, res) => {
     try {
         const { email } = req.query;
         const response = await checkIfUserExists(email);
+        console.log(response);
         res.send(response);
     } catch (error) {
         const response = { error: error.message };
@@ -36,5 +37,5 @@ app.get('/check-user', async (req, res) => {
 
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`App listening at http://localhost:${port}`);
 });
